1fad65d9c845b4c0737c2bcb8ae60936d04126d8
TomNash <nashtf@g.cofc.edu>
2018-08-12 16:36:44 -0400

    Working models and Dockerfile

--- app.py
+++ app.py
@@ -1,76 +1,96 @@
+#!/usr/bin/env python3
+
+from keras.models import load_model
+from keras.preprocessing import sequence
+from flask import Flask, request
+from flask_restful import Resource, Api
+import tensorflow as tf
+from keras import backend as K
+import json
+
+host = 'http://127.0.0.1'
+port = 5000
+
+app = Flask(__name__)
+api = Api(app)
+
 class ScoreModel(Resource):
     def __init__(self):
         # set file paths
         self.model_file_path = 'models/imdb_sentiment_RNN.h5'
-        self.weights_file_path = "models/weights.best.hdf5"
-        self.tokenizer_file_path = 'models/tokenizer.pkl'
-
-        # loading the tokenizer
-        with open(self.tokenizer_file_path, 'rb') as handle:
-            tokenizer = pickle.load(handle)
-
-        # load the model json
-        with open(self.model_file_path, 'rb') as f:
-            model_json = pickle.load(f)
-
         # create the model from json and load the weights
-        model_loaded = model_from_json(model_json)
-        model_loaded.load_weights(self.weights_file_path, by_name=False)
-        self.tokenizer = tokenizer
-        self.model = model_loaded
+        self.model = load_model(self.model_file_path)
+
+    def preprocess(self, data):
+        '''
+
+        :param data:
+        :return:
+        '''
+        seq_length = self.model.input_shape[1]
+        index_from = 3  # word index offset
+        with open('data/imdb_word_index.json') as f:
+            word_to_id = json.load(f)
+        word_to_id = {k: (v + index_from) for k, v in word_to_id.items()}
+        word_to_id["<PAD>"] = 0
+        word_to_id["<START>"] = 1
+        word_to_id["<UNK>"] = 2
+        # Add a start sequence to the string and map unknown keys to {2: "<UNK>"}
+        seq = [1] + [word_to_id.get(x.lower() if x not in ('<PAD>', '<START>', '<UNK>') else x, 2) for x in data.split()]
+        # If word is outside the range of words used for model, then cast to unknown
+        seq = [[x if x <= 5000 else 2 for x in seq]]
+        # Pad sequence
+        input_sequence = sequence.pad_sequences(seq, maxlen=seq_length, padding='pre')
+        return input_sequence
 
     def predict(self, data):
         '''
-        Core scoring function for the model
-        :param json_string: with a key of 'text' where the text features are extracted
-        :return: json_string of predictions with
-        keys: 'non_toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate
+
+        :param data:
+        :return:
         '''
-        if type(data) is str:
-            data = [data]
-        features = self.tokenizer.texts_to_matrix(data)
-        preds = self.model.predict(features)
-        keys = ['non -toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-        output = dict(zip(keys, preds.transpose().tolist()))
-        return output
+        input_sequence = self.preprocess(data)
+        preds = self.model.predict(input_sequence)
+        return 'Positive review' if preds[0][0] >= 0.5 else 'Negative review'
 
 
 @app.route('/')
 def api_root():
+    """
+
+    :return:
+    """
     return 'RestAPI for predictive model, to test'
 
 
 @app.route('/echo', methods=['POST'])
 def echo():
+    """
+
+    :return:
+    """
     json_data = request.get_json()
     text = json_data.get('text')
-    return 'Rest API Pass through :' + text
+    return 'Rest API Pass through: %s\n' % (text)
 
 
 @app.route('/score', methods=['POST'])
 def score_model():
-    from keras.models import model_from_json
-    from keras.preprocessing.text import Tokenizer
-    import pickle
-    import tensorflow
-    import tensorflow as tf
-    from keras import backend as K
+    """
 
+    :return:
+    """
     config = tf.ConfigProto(allow_soft_placement=True, device_count={'CPU': 1, 'GPU': 0})
     session = tf.Session(config=config)
     K.set_session(session)
     json_data = request.get_json()
     text = json_data.get('text')
-    if type(text) is str:
-        text = [text]
-    else:
-        s = ScoreModel()
-        output_dict = s.predict(text)
-        return json.dumps(output_dict)
+    s = ScoreModel()
+    return s.predict(text) + "\n"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
 # test api from command line
 # curl -H "Content-Type: application/json" -X POST -d "{\"text\":\"this is a test\"}" http://127.0.0.1:5000/score
